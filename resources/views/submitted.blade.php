738dd56e061f134d2a7925070ca87ba0d583d89d
Hanifi Abrar Setiawan <115058216+HanifiSetiawan@users.noreply.github.com>
2023-10-04 11:31:42 +0700

    Update submitted.blade.php

--- resources/views/submitted.blade.php
+++ resources/views/submitted.blade.php
@@ -5,29 +5,152 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <title>Submitted Data</title>
 </head>
-<body>
-    <h1>Submitted Data</h1>
-    
-    <p>Name: {{ $data['name'] }}</p>
-    <p>Email: {{ $data['email'] }}</p>
-    <p>Address: {{ $data['address'] }}</p>
-    <p>Score: {{ $data['score'] }}</p>
+<body class="bg-dark">
+    <section>
+    <div class="container mt-5">
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="card" style="width: 50rem;">
+                <div class="card-header text-center">
+                    <h1 class="card-title">Submitted Data</h1>
+                </div>
+                <div class="card-body">
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Name</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['name'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>ID</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['id'] }}</p>
+                            </div>
+                        </div>
+                    </div>
 
-    <!-- Display the uploaded image if it's stored -->
-    @if (isset($data['picture_path']))
-        <img src="{{ asset($data['picture_path']) }}" alt="Uploaded Picture">
-    @endif
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Phone number</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['phonenumber'] }}</p>
+                            </div>
+                        </div>
+                    </div>
 
-    <br><br>
-    <a class="btn btn-outline-primary" href="/form" role="button" >Go back</a>
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Address</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['address'] }}</p>
+                            </div>
+                        </div>
+                    </div>
 
-    <br><br><br>
-    
-    <h1 class="text-center text-success">SUCCESS !!!!</h1>
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Age</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['age'] }}</p>
+                            </div>
+                        </div>
+                    </div>
 
-    
-   
-    
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Religion</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['religion'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Gender</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['gender'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Job</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['job'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Job</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['job'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Marial Status</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['marialstatus'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="border border-dark px-3 pt-2 rounded mb-2">
+                        <div class="d-flex justify-content-between align-items-center border-succes">
+                            <div class="text-left">
+                                <p>Citizenship</p>
+                            </div>
+                            <div class="text-right">
+                                <p>{{ $data['citizenship'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Display the uploaded image if it's stored -->
+                    @if (isset($data['picture_path']))
+                        <img src="{{ asset($data['picture_path']) }}" alt="Uploaded Picture">
+                    @endif
+
+                    <br><br>
+
+                    <a class="btn btn-outline-primary btn-lg" href="/form" role="button" >Go back</a>
+
+                    <h1 class="text-center text-success">SUCCESS !!!!</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
