dcf045d84c8d7cad9c337000b5ac0206c67653a7
Hanifi Abrar Setiawan <115058216+HanifiSetiawan@users.noreply.github.com>
2023-10-04 11:31:02 +0700

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,45 +1,139 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+ 
     <title>Form Validation</title>
 </head>
-<body>
-    <h1>This Is a Simple Form</h1>
+<body class="bg-dark">
+    <section class="bg-dark">
+        <div class="container mt-5 h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class= "card border-dark mb-3" style="width: 50rem;">
+                    <div class="card-header text-center">
+                        <h1 class="card-title">Citizenship Form</h1>
+                    </div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+                        <form method="POST" action="/submit" enctype="multipart/form-data">
+                            @csrf
 
-    <form method="POST" action="/submit" enctype="multipart/form-data">
-        @csrf
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="name" id="name" placeholder="Please input your full name" value="{{ old('name') }}" required>
+                                    </div>
+                                </div>
+                            </div>
 
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" value="{{ old('name') }}" required>
-        <br><br>
+                            <div class="mb-3">
+                                <label for="id" class="form-label">ID</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="id" id="id" placeholder="Please input your specific ID" value="{{ old('ID') }}" required>
+                                    </div>
+                                </div>
+                            </div>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" value="{{ old('email') }}" required>
-        <br><br>
+                            <div class="mb-3">
+                                <label for="phonenumber" class="form-label">Phonenumber</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="phonenumber" id="phonenumber" placeholder="Please input your Phonenumber" value="{{ old('phonenumber') }}">
+                                    </div>
+                                </div>
+                            </div>
 
-        <label for="address">Address:</label>
-        <input type="text" name="address" id="address" value="{{ old('address') }}" required>
-        <br><br>
+                            <div class="mb-3">
+                                <label for="address" class="form-label">Address</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="address" id="address" placeholder="Please input your Address" value="{{ old('address') }}">
+                                    </div>
+                                </div>
+                            </div>
 
-        <label for="score">Score (2.50 - 99.99):</label>
-        <input type="number" step="0.01" name="score" id="score" value="{{ old('score') }}" required>
-        <br><br>
+                            <div class="mb-3">
+                                <label for="age" class="form-label">Age</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="age" id="age" placeholder="Please input your Age" value="{{ old('age') }}">
+                                    </div>
+                                </div>
+                            </div>
 
-        <label for="picture">Picture (Max 2MB):</label>
-        <input type="file" name="picture" id="picture" required>
-        <br><br>
+                            <div class="mb-3">
+                                <label for="religion" class="form-label">Religion</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="religion" id="religion" placeholder="Please input your Religion" value="{{ old('religion') }}">
+                                    </div>
+                                </div>
+                            </div>
 
-        <button type="submit">Submit</button>
-    </form>
+                            <div class="mb-3">
+                                <label for="gender" class="form-label">Gender</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="gender" id="gender" placeholder="Please input your Gender" value="{{ old('gender') }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="job" class="form-label">Job</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="job" id="job" placeholder="Please input your Job" value="{{ old('job') }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="marialstatus" class="form-label">Marial status</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="marialstatus" id="marialstatus" placeholder="Please input your Marial status" value="{{ old('marialstatus') }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="citizenship" class="form-label">Citizenship</label>
+                                <div class="row g-3">
+                                    <div class="col-md">
+                                        <input type="text" class="form-control" name="citizenship" id="citizenship" placeholder="Please input your Citizenship" value="{{ old('citizenship') }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="picture" class="form-label">Picture (Max 2MB):</label>
+                                <input type="file" name="picture" id="picture" required>
+                            </div>
+
+                            <div class="card-footer text-body-secondary justify-content-center align-items-center">
+                                <button type="submit" class="btn btn-outline-primary btn-lg">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
